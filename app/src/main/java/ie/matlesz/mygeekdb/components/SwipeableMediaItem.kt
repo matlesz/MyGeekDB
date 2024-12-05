b4506dd05f1e2fad7852c6f2cf9274503d3fbea2
matlesz <matlesz@gmail.com>
2024-12-05 01:30:59 +0000

    cleaning up the codebase

--- app/src/main/java/ie/matlesz/mygeekdb/components/SwipeableMediaItem.kt
+++ app/src/main/java/ie/matlesz/mygeekdb/components/SwipeableMediaItem.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import ie.matlesz.mygeekdb.views.FavoriteItemCard
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
